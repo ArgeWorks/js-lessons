2c6b30309e4bc79257c964e2e2be8bf423612497
ArgeWorks <argeworks@icloud.com>
2018-05-11 20:27:01 +0300

    Home Work №2 completed

--- js/script.js
+++ js/script.js
@@ -1,91 +1,109 @@
-// ========== Задачи - var ========== //
+// ========== Присваивание ========== //
+
+//1) Записи в коротком виде
+let a = b = c = 5,
+    x = y = i = z = 10;
+
+a += 10, 
+b *= 18, 
+c -= 10,
+x += a,
+y *= z,
+i *= y * 5;
+
+//2) Возмедение переменной в квадрат
+let sourceNumb = 34;
+
+console.log(`Исходное число: ${sourceNumb}, в квадрате: ${sourceNumb *= sourceNumb}`);
+
+// ========== Арифметические операторы ========== //
+
+//1) Сравнить записи
+let a1 = a2 = 5;
+
+console.log(`Выполняем a1++ и ++a2, на этой строке полчаем: a1 = ${a1++}, a2 = ${++a2}`);
+console.log(`На следующей строке: a1 = ${a1}, a2 = ${a2}`);
+
+//2) Узнаем четное или не четное с помощью оператора %
+sourceNumb = 9;
+
+if (sourceNumb % 2 == 1) 
+    console.log(`Исходное число: ${sourceNumb} и оно не четное`);
+else 
+    console.log(`Исходное число: ${sourceNumb} и оно четное`);
+
+// ========== Условные операторы ========== //
 
 //1)
-let price;
-const maxNumber = 10;
-const userName = 'Igor';
-let userInfo;
+let overflow = 'hidden';
 
-//2) Результат - undefined
+if (overflow == 'hidden') overflow = 'visible';
+else overflow = 'hidden';
 
-//3) Результат - string 2
-
-// ========== Задачи let и const ========== //
-
-//1) Результат - test is not defined
-
-//2) Результат - Assignment to constant variable.
-
-//3) Результат - Identifier 'Somevariable' has already been declared
-
-// ========== Задачи string ========== //
-
-let str = 'some test string';
-console.log(`Исходный текст: ${str}`);
-
-//1)
-console.log(`Первая буква: ${str[0]}, последняя буква: ${str[str.length-1]}`);
+overflow = overflow == 'hidden' ? 'visible' : 'hidden';
 
 //2)
-console.log(`Первая буква в верхнем регистре: ${str[0].toUpperCase()}, последняя буква в верхнем регистре: ${str[str.length - 1].toUpperCase()}`);
+let degrees1 = 0;
 
-//3)
-console.log(`Положение(индекс) слова strange: ${str.indexOf('string')}`);
+if (degrees1 == 0) degrees1 = 1;
+else if (degrees1 < 0) degrees1 = 'меньше нуля';
+else degrees1 *= 10;
 
-//4) Сомниваюсь в этом варианте, если пробелов будет больше 2-х то будет возвращен индекс последнего пробела.
-console.log(`Положение второго пробела : ${str.lastIndexOf(' ')}`);
+console.log(`В холодильнике градусов: ${degrees1}`);
 
-//5)
-console.log(`Получить строку с 5-го символа длиной 4 буквы: ${str.substr(5, 4)}`);
+// Тернарный вариант
+let degrees2 = -1;
 
-//6)
-console.log(`Получить строку с 5-го по 9-й символ: ${str.substring(5, 9)}`);
+degrees2 = degrees2 == 0 ? 1 : 
+            degrees2 < 0 ? 'меньше нуля' : degrees2 *= 10;
 
-//7)
-console.log(`Строка без последних 6 символов: ${str.slice(0, -6)}`);
+console.log(`В морозильнике градусов: ${degrees2}`);
 
-//8)
-let a = 20,
-    b = 18;
+// ========== Switch Case ========== //
 
-console.log(`Док, в какой год мы попали ? - В ${a + '' + b}, Марти.`);
+let display = 'none';
 
-// ========== Задачи int ========== //
+console.log(`Свойство display равно: ${CheckDisplay(display)}`);
 
+function CheckDisplay(inputParam) {
+    switch (inputParam) {
+        case 'block': 
+            return 'block';
+        case 'none': 
+            return 'none';
+        case 'inline': 
+            return 'inline';
+        default: 
+            return 'flex';
+    }
+};
+
+// ========== Преобразование типов ========== //
 //1)
-console.log(`Получить число Пи из Math и округлить его до 2 знаков после точки: ${Math.PI.toFixed(2)}`);
+let value;
+
+// Оператор "ИЛИ ||" ищет правду и возвращает первое правдивое значение, если все значения ложные, возвращается последнее значение.
+// Оператор "И &&" ищет ложь и возвращает первое ложное значение, если все значения правдивые, возвращается последнее значение.
+value = 0 || 'string'; // value = string, 0 = false, не пустая строка = true.
+value = 1 && 'string'; // value = string, оба значения = true, возвращается последнее.
+value = null || 25; // value = 25, null = false, 25 = true.
+value = null && 25; // value = null, null = false, 25 = true.
+value = null || 0 || 25; // value = 25, null = false, 0 = false, 25 = true.
+value = null && 0 && 25; // value = null, null = false, 0 = false, 25 = true.
 
 //2)
-console.log('Найти максимальные и минимальные числа из набора (15, 11, 16, 12, 51, 12, 13, 51)');
-console.log(`Максимальное число: ${Math.max(15, 11, 16, 12, 51, 12, 13, 51)}`);
-console.log(`Минимальное число: ${Math.min(15, 11, 16, 12, 51, 12, 13, 51)}`);
+// Сложение любого значения со строкой выполняет конкатенацию и конвертирование в строку без сложения.
+// При любых других арифметических операциях строка будет пытаться сконвертироваться в число после чего будет выполнена арифметическая операция.
+// При добавлении унарного +, строка пытается преобразоваться в цифру.
+// true равен 1.
+// null, false равны 0.
+// undefined равен Not a Number (NaN).
 
-//3)
-console.log(`Получить случайное число и округлить его до двух цифр после запятой: ${Math.random().toFixed(2)}`);
-
-let maxNumb = 100;
-console.log(`Получить случайное число от 0 до ${maxNumb}: ${(Math.random() * maxNumb).toFixed()}`);
-
-//4)
-console.log(`Результат вычисления 0.6 + 0.7 = ${(0.6 + 0.7).toFixed(1)}`);
-
-//5)
-let myMoney = '100$';
-console.log(`Получить число из строки ${myMoney} = ${parseInt(myMoney)}`);
-
-// ========== Задачи objects ========== //
-
-//1)
-let marketItem = {
-    product: 'iPhone'
-}
-
-//2)
-marketItem.price = 1000;
-marketItem.currency = 'dollar';
-
-//3)
-marketItem.details = {
-    model: 'iPhone X',
-    color: 'Black'
-}
+12 + 14 + '12' // = '2612'
+3 + 2 - '1' // = 4
+'3' + 2 - 1 // = '31'
+true + 2 // = 3
++'10' + 1 // = 11
+undefined + 2 // = Nan
+null + 5 // = 5
+true + undefined // = NaN
