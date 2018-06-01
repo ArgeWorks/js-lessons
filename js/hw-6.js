19644c5e52e872bffe19f1434c2d32390c7f28c2
ArgeWorks <argeworks@icloud.com>
2018-06-01 22:15:51 +0300

    Small changes in Home Work №6

--- js/hw-6.js
+++ js/hw-6.js
@@ -13,19 +13,22 @@ let tasks = [
 const ul = document.querySelector('.list-group');
 
 // Add all tasks to HTML markup
-tasks.forEach(task => ul.insertAdjacentElement('afterbegin', listTemplate(task)));
+tasks.forEach(task => ul.insertAdjacentElement('afterbegin', createElement('li', ['list-group-item'], task.text, task.id)));
 
-function listTemplate(task) {
+//Elements template
+function createElement(tag, classes, text, id) {
     // Create element
-    const li = document.createElement('li');
-    // Paste text to element
-    li.textContent = task.text;
+    const element = document.createElement(tag);
+    // Set classes
+    for (let elementClass of classes) {
+        element.classList.add(elementClass);
+    }
     // Set id
-    li.setAttribute('data-id', task.id);
-    // Set class
-    li.classList.add('list-group-item');
+    if (id !== undefined) element.setAttribute('data-id', id);
+    // Paste text to element
+    element.textContent = text;
     // Return element
-    return li;
+    return element;
 }
 
 // Add task
@@ -47,12 +50,10 @@ function deleteTask(id) {
         if (task.id === String(id))
             tasks.splice(i, 1);
     });
-
     // Delete task at HTML markup
     for (let task of document.getElementsByClassName('list-group-item')) {
         if (task.dataset.id === String(id)) ul.removeChild(task);
     }
-
     // Show info message
     messageInfo('Task successfully removed!');
 }
@@ -61,17 +62,9 @@ function deleteTask(id) {
 function messageInfo(text) {
     // Get alert element
     const divAlert = document.querySelector('.alert');
-
     // Create alert element and add text existing alert
     if (divAlert === null) {
-        // Create element
-        const divAlert = document.createElement('div');
-        // Add classes
-        divAlert.classList.add('alert', 'alert-info');
-        // Add message
-        divAlert.textContent = text;
-        // Paste element to HTML markup
-        document.querySelector('.container').insertAdjacentElement('afterbegin', divAlert);
+        document.querySelector('.container').insertAdjacentElement('afterbegin', createElement('div', ['alert', 'alert-info'], text));
     } else {
         // Or add message to existing elert
         divAlert.textContent = text;
