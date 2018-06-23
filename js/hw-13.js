95fb7c2fea8957a2ff8e4f9061ab6eee4eea29ab
ArgeWorks <argeworks@icloud.com>
2018-06-23 11:42:42 +0300

    Home Work №13 small changes

--- js/hw-13.js
+++ js/hw-13.js
@@ -1,11 +1,15 @@
+// Get elements
+let usersList = document.querySelector('.users-list'),
+    btnGetUsers = document.querySelector('.btn-get-users'),
+    btnSortUsers = document.querySelector('.btn-sort-users'),
+    usersArr = [];
+
 // UI class
 class UI {
     addUserToList(user) {
-        // Get users list
-        const usersList = document.querySelector('.users-list');
         // Create markup
         const userCard = `
-            <div class="user-card">
+            <div class="user-card" data-id="${user.id}">
                 ${user.name}
                 <div class="user-info">
                     <span class="user-full-name">Name: ${user.name}</span>
@@ -19,24 +23,27 @@ class UI {
         usersList.insertAdjacentHTML('beforeend', userCard);
     }
 
+    addInfoToList(msg) {
+        // Create markup
+        const infoCard = `
+            <div class="info-card">
+                ${msg}
+            </div>
+        `;
+
+        usersList.insertAdjacentHTML('beforeend', infoCard);
+    }
+
     clearUsersList() {
         if (document.querySelector('.info-card')) document.querySelector('.info-card').remove();
         document.querySelectorAll('.user-card').forEach(item => item.remove());
     }
 
-    changeBtnState(state) {
-        if (state === 'load') {
-            btnGetUsers.textContent = 'Loading...';
-            btnGetUsers.disabled = true;
-        } else {
-            btnGetUsers.textContent = 'Get Users';
-            btnGetUsers.disabled = false;
-        }
+    changeBtnGetUsersState(state) {
+        btnGetUsers.textContent = (state === 'load') ? btnGetUsers.dataset.loadingText : btnGetUsers.dataset.defaultText;
+        btnGetUsers.disabled = (state === 'load') ? true : false;
     }
 }
-
-let usersList = document.querySelector('.users-list'),
-    btnGetUsers = document.querySelector('.btn-get-users');
 
 // Add users to markup
 btnGetUsers.addEventListener('click', function (e) {
@@ -44,28 +51,45 @@ btnGetUsers.addEventListener('click', function (e) {
     const http = new Http();
     // Create UI
     const ui = new UI();
-    // Change button state
-    ui.changeBtnState('load');
-
+    // Change get users button state
+    ui.changeBtnGetUsersState('load');
     // Send get request
     http.get("https://jsonplaceholder.typicode.com/users", function (err, res) {
         // Check error
         if (err) {
-            document.querySelector('.info-card').textContent = err;
-            // Change button state
-            ui.changeBtnState();
+            // Clear users list
+            ui.clearUsersList();
+            // Show status info
+            ui.addInfoToList(err);
+            // Change get users button state
+            ui.changeBtnGetUsersState();
+            // Disable sort button
+            btnSortUsers.disabled = true;
             return;
         }
-        // Clear users list
-        ui.clearUsersList();
         // Parse result
-        let usersArr = JSON.parse(res);
-        // Check isArray and add all users to markup
-        if (Array.isArray(usersArr)) usersArr.forEach(user => ui.addUserToList(user));
-        // Change button state
-        ui.changeBtnState();
+        usersArr = JSON.parse(res);
+        // Users count
+        if (usersArr.length) {
+            // Clear users list
+            ui.clearUsersList();
+            // Sort users by alphabet
+            usersArr.sort((prev, next) => prev.name > next.name);
+            // Check isArray and add all users to markup
+            if (Array.isArray(usersArr)) usersArr.forEach(user => ui.addUserToList(user));
+            // Change get users button state
+            ui.changeBtnGetUsersState();
+            // Enable sort button
+            btnSortUsers.disabled = false;
+        } else {
+            // Clear users list
+            ui.clearUsersList();
+            // Show status info
+            ui.addInfoToList('Users list is empty');
+            // Disable sort button
+            btnSortUsers.disabled = true;
+        }
     });
-
 });
 
 // Show or hide user info
@@ -81,9 +105,22 @@ usersList.addEventListener('click', e => {
         // Show user info
         e.target.children[0].classList.toggle('d-block');
     }
-
     // Hide user info
     if (e.target.classList.contains('icon-close-info')) {
         e.target.parentElement.classList.remove('d-block');
+    }
+});
+
+// Sort users
+btnSortUsers.addEventListener('click', () => {
+    if (usersArr.length) {
+        // Reverse users array
+        usersArr.reverse();
+        // Create UI
+        const ui = new UI();
+        // Clear users list
+        ui.clearUsersList();
+        // Add all users to markup
+        usersArr.forEach(user => ui.addUserToList(user));
     }
 });
